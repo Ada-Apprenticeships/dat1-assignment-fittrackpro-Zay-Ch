6499245adbc2830fa9986d70cf6975f433153e0f
Zay-Ch <zaynab.choudhury@ada.ac.uk>
2025-01-31 13:51:54 +0000

    staff id to be trainer id

--- src/staff_management.sql
+++ src/staff_management.sql
@@ -19,11 +19,12 @@ ORDER BY position;
 -- 2. Find trainers with one or more personal training session in the next 30 days
 -- TODO: Write a query to find trainers with one or more personal training session in the next 30 days
 
-SELECT s.staff_id, CONCAT(s.first_name, ' ', s.last_name) AS trainer_name, COUNT(pt.session_id) AS session_count
+SELECT s.staff_id AS trainer_id, CONCAT(s.first_name, ' ', s.last_name) AS trainer_name, COUNT(pt.session_id) AS session_count
 FROM staff s
 JOIN personal_training_sessions pt ON s.staff_id = pt.staff_id
 WHERE pt.session_date BETWEEN CURRENT_DATE AND DATE(CURRENT_DATE, '+30 days')
 GROUP BY s.staff_id, trainer_name;
 
+-- Sets staff id to be trainer id, to fulfill the read me requirement
 -- Counts trainers with upcoming personal training sessions.
 
