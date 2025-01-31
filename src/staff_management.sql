36738c6d808b438e413e41972d416cd1ed8470e4
Zay-Ch <zaynab.choudhury@ada.ac.uk>
2025-01-31 13:32:41 +0000

    staff_management done

--- src/staff_management.sql
+++ src/staff_management.sql
@@ -11,5 +11,19 @@ PRAGMA foreign_keys = ON;
 -- 1. List all staff members by role
 -- TODO: Write a query to list all staff members by role
 
+SELECT staff_id, first_name, last_name, position 
+FROM staff
+ORDER BY position;
+
+
 -- 2. Find trainers with one or more personal training session in the next 30 days
 -- TODO: Write a query to find trainers with one or more personal training session in the next 30 days
+
+SELECT s.staff_id, CONCAT(s.first_name, ' ', s.last_name) AS trainer_name, COUNT(pt.session_id) AS session_count
+FROM staff s
+JOIN personal_training_sessions pt ON s.staff_id = pt.staff_id
+WHERE pt.session_date BETWEEN CURRENT_DATE AND DATE(CURRENT_DATE, '+30 days')
+GROUP BY s.staff_id, trainer_name;
+
+-- Counts trainers with upcoming personal training sessions.
+
