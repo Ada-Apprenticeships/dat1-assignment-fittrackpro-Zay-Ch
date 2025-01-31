76f8ef6dbf7daca060253595871b79f7a79ae585
Zay-Ch <zaynab.choudhury@ada.ac.uk>
2025-01-31 14:06:42 +0000

    Personal training task finished

--- src/personal_training.sql
+++ src/personal_training.sql
@@ -9,3 +9,12 @@ PRAGMA foreign_keys = ON;
 
 -- 1. List all personal training sessions for a specific trainer
 -- TODO: Write a query to list all personal training sessions for a specific trainer
+
+SELECT pt.session_id, CONCAT(m.first_name, ' ', m.last_name) AS member_name, 
+       pt.session_date, pt.start_time, pt.end_time
+FROM personal_training_sessions pt
+JOIN members m ON pt.member_id = m.member_id
+JOIN staff s ON pt.staff_id = s.staff_id
+WHERE CONCAT(s.first_name, ' ', s.last_name) = 'Ivy Irwin'
+ORDER BY pt.session_date, pt.start_time;
+
